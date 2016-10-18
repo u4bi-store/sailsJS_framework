304adb6d5dfbcf700359ca7e800c66732bf9a7aa
u4bi <rootcode10@gmail.com>
2016-10-18 18:00:07 +0900

    MYSQL db init

--- sailsMysql_2/config/models.js
+++ sailsMysql_2/config/models.js
@@ -1,32 +1,11 @@
-/**
- * Default model configuration
- * (sails.config.models)
- *
- * Unless you override them, the following properties will be included
- * in each of your models.
- *
- * For more info on Sails models, see:
- * http://sailsjs.org/#!/documentation/concepts/ORM
- */
-
 module.exports.models = {
-
-  /***************************************************************************
-  *                                                                          *
-  * Your app's default connection. i.e. the name of one of your app's        *
-  * connections (see `config/connections.js`)                                *
-  *                                                                          *
-  ***************************************************************************/
-  // connection: 'localDiskDb',
-
-  /***************************************************************************
-  *                                                                          *
-  * How and whether Sails will attempt to automatically rebuild the          *
-  * tables/collections/etc. in your schema.                                  *
-  *                                                                          *
-  * See http://sailsjs.org/#!/documentation/concepts/ORM/model-settings.html  *
-  *                                                                          *
-  ***************************************************************************/
-  // migrate: 'alter'
-
+  connection: 'someMysqlServer',
+  migrate: 'drop'
 };
+
+/*
+drop:  서버를 죽였다가 다시 띄울 때마다 테이블을 다시 날려버리는 옵션입니다. 개발할 때 좋음.
+alter: 서버를 죽였다가 다시 띄울 때 모델에 칼럼의 변경사항을 체크해서
+       업데이트 해주는 옵션임. 데이터는 살아있음.
+safe:  서버를 죽였다가 다시 띄워도 아무것도 하지 않음.
+*/
