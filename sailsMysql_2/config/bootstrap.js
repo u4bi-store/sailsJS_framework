10b431d9a65065435d1bed72539d68bc3d04a0ac
u4bi <rootcode10@gmail.com>
2016-10-18 18:28:31 +0900

    db insert

--- sailsMysql_2/config/bootstrap.js
+++ sailsMysql_2/config/bootstrap.js
@@ -4,4 +4,5 @@ module.exports.bootstrap = function(cb) {
   U4bi.create({name: '명삼'}).exec(console.log);
   U4bi.create({name: '명훈'}).exec(console.log);
   U4bi.create({name: '명한'}).exec(console.log);
+  cb();
 };
